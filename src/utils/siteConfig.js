982808e232828bf68e6c10f9c7ac8f466f8fef85
jonmccon <jonmccon@gmail.com>
2019-03-03 23:05:32 -0800

    Initial commit

---
+++ src/utils/siteConfig.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteTitle: 'Work by Night',
+  siteTitleAlt: 'Work by Night', // This allows an alternative site title for SEO schema.
+  publisher: 'Jonny McConnell', // Organization name used for SEO schema
+  siteDescription:
+    'A designer and photographer in the Pacific Northwest',
+  siteUrl: 'https://workbynight.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+  postsPerHomePage: 12, // Number of posts shown on the 1st page of of the index.js template (home page)
+  postsPerPage: 12, // Number of posts shown on paginated pages
+  author: 'Jonny McConnell', // Author for RSS author segment and SEO schema
+  authorUrl: 'http://beardseyeview.com', // URL used for author and publisher schema, can be a social profile or other personal site
+  userTwitter: '@jonmccon', // Change for Twitter Cards
+  shortTitle: 'Workbynight', // Used for App manifest e.g. Mobile Home Screen
+  shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
+  shareImageWidth: 400, // Change to the width of your default share image
+  shareImageHeight: 400, // Change to the height of your default share image
+  siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
+  backgroundColor: '#e9e9e9', // Used for Offline Manifest
+  themeColor: '#121212', // Used for Offline Manifest
+  copyright: 'Copyright © 2018 Jonny McConnell', // Copyright string for the RSS feed
+}
